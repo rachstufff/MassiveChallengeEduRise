3c2254be273486f2bb4dc8a2bf4e24f93078cd37
22091397034_Khofifah Wulandari <124429174+22091397034KhofifahWulandari@users.noreply.github.com>
2025-05-28 12:36:44 +0700

    Update Eduevent.jsx

--- src/pages/Eduevent/Eduevent.jsx
+++ src/pages/Eduevent/Eduevent.jsx
@@ -1,8 +1,8 @@
 // src/pages/Eduevent/Eduevent.jsx
 import React from 'react';
 import HeroSection from "../../components/eduevent/HeroSection";
-import CompetitionSection from "../../components/eduevent/CompetitionSection"; // Path sudah diperbaiki jika diperlukan
-import WebinarSection from "../../components/eduevent/WebinarSection";     // Path sudah diperbaiki jika diperlukan
+import CompetitionSection from "../../components/eduevent/CompetitionSection"; 
+import WebinarSection from "../../components/eduevent/WebinarSection";     
 
 const Eduevent = () => {
   return (
