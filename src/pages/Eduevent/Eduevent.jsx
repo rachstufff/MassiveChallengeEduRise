331064e0fca1d697e532dfb9db2463c58a5dbb29
22091397034_Khofifah Wulandari <124429174+22091397034KhofifahWulandari@users.noreply.github.com>
2025-05-28 14:02:04 +0700

    Update Eduevent.jsx

--- src/pages/Eduevent/Eduevent.jsx
+++ src/pages/Eduevent/Eduevent.jsx
@@ -1,15 +1,19 @@
 // src/pages/Eduevent/Eduevent.jsx
 import React from 'react';
+import Navbar from '../../components/Navbar'; 
+import Footer from '../../components/Footer'; 
 import HeroSection from "../../components/eduevent/HeroSection";
-import CompetitionSection from "../../components/eduevent/CompetitionSection"; 
-import WebinarSection from "../../components/eduevent/WebinarSection";     
+import CompetitionSection from "../../components/eduevent/CompetitionSection";
+import WebinarSection from "../../components/eduevent/WebinarSection";
 
 const Eduevent = () => {
   return (
     <div>
-      <HeroSection /> 
+      <Navbar /> {/* Menambahkan Navbar di sini */}
+      <HeroSection />
       <CompetitionSection />
       <WebinarSection />
+      <Footer /> {/* Menambahkan Footer di sini */}
     </div>
   );
 };
